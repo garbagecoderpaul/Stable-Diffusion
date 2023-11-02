85e7c84c0060351881af02f03bf90c0c1b1be9dd
Garbage Coder <48788790+garbagecoderpaul@users.noreply.github.com>
2023-11-02 21:52:50 +0000

    Add files via upload

---
+++ train_DBs_crop.sh
@@ -0,0 +1,15 @@
+# Prep list of CharacterIDs from destination folder
+cd /workspace/train_img/
+
+ls -h > /workspace/Stable-Diffusion/characters.txt
+
+
+cd /workspace/Stable-Diffusion/
+
+# Install virtual environment
+source /workspace/kohya_ss/venv/bin/activate
+# alias activate="/workspace/kohya_ss/venv/bin/activate"
+
+# Run the command
+## Input = characters.txt
+python train_DBs_bucket.py
