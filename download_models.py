26a997e2583fa8336baba837c15fac0dae3c5ca0
Garbage Coder <48788790+garbagecoderpaul@users.noreply.github.com>
2023-12-04 09:56:52 +0000

    Add files via upload

--- download_models.py
+++ download_models.py
@@ -1,19 +1,13 @@
 import subprocess
 import re
 import csv
+import os
 
 # function
 def extract_fileid(input_string):
-    # Define a regular expression pattern to match the desired part
-    # pattern = r'/folders/([a-zA-Z0-9_-]+)'
     pattern = r"/d/([^/]+)/"
-
-    # Use re.search to find the match in the input string
     match = re.search(pattern, input_string)
-
-    # Check if a match is found
     if match:
-        # Extract the desired part (group 1)
         fileid = match.group(1)
         return fileid
     else:
@@ -22,8 +16,13 @@ def extract_fileid(input_string):
 
 # prep command
 def prep_command(file_id):
-    command = 'gdrive files download ' + file_id + ' --destination /workspace/stable-diffusion-webui/models/Stable-diffusion'
+    command = 'gdrive files download ' + file_id + ' --destination /workspace/stable-diffusion-webui/models/Stable-diffusion/model'
     return command
+
+# Check if directory exists, if not, create it
+directory = '/workspace/stable-diffusion-webui/models/Stable-diffusion/model'
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 # Prep a list of model's file_id list
 file_id_list = []
@@ -33,10 +32,8 @@ with open('gen.csv', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     for item in reader:
         url = item[0]
-        print(url)
         file_id = extract_fileid(url)
         file_id_list.append(file_id)
-        print('file id is \n',  file_id)
 
 # Run the gdrive command using subprocess and capture the output
 for item in file_id_list:
